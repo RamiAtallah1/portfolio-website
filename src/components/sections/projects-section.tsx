a9f67538b0ea874e500d42f020cec3fff8409b65
RamiAtallah1 <ramiatallah63@gmail.com>
2026-07-12 18:33:54 +0300

    fix: add mobile side padding to projects and style cards with gradient border

--- src/components/sections/projects-section.tsx
+++ src/components/sections/projects-section.tsx
@@ -7,7 +7,7 @@ export default function ProjectsSection() {
       <h2 className="text-blue-600 text-3xl text-center font-bold mb-4 pt-20">
         Projects
       </h2>
-      <div className="py-12 flex flex-wrap gap-8 justify-center">
+      <div className="py-12 px-4 md:px-8 flex flex-wrap gap-8 justify-center">
         {projects &&
           projects.map((project, i) => (
             <ProjectCard key={i} project={project} />
